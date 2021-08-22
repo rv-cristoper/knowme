19dc8e83d31475b243135e537f2936c23c20ca72
cristoper <cristoper.runco@gmail.com>
2021-08-21 22:44:17 -0500

    feat: add layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,31 @@
 import Head from 'next/head'
 import Layout from '../components/layout/Layout'
-import MyApp from './_app'
-// import '../styles/globals.css'
+
+import styles from '../styles/asdf.module.scss'
 
 const App = (): JSX.Element => {
+
    return (
       <>
          <Head>
             <title>rv_cristoper</title>
          </Head>
          <Layout>
-            xd
+            <div className={styles.main}>
+               Nombre
+               <div className={styles.apellido}>
+                  Apellido
+                  <div>
+                     Edad
+                     <div>
+                        Cumpleaños
+                     </div>
+                     <div>
+                        Nacimiento
+                     </div>
+                  </div>
+               </div>
+            </div>
          </Layout>
 
       </>
